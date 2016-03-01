1111f8b0970b9d20ce8090f78d1bbff7f861b1b9
edicas666 <edicas666@gmail.com>
2016-03-01 09:21:01 -0600

    version2.2222

--- login.php
+++ login.php
@@ -2,12 +2,11 @@
   include_once "funciones.php";
   $user = $_POST['user'];
   $pass = $_POST['pass'];
-
   $link = Conectarse();
   //$pass = sha1(md5($pass));
   $query = sprintf("SELECT user FROM usuarios WHERE user='%s' AND pass='%s'", $user, $pass);
   $result = mysql_query($query,$link);
-  
+
   if(mysql_num_rows($result) != 0){
     echo "<br><h1>Bienvenido ".mysql_fetch_assoc($result)['user']."</h1>";
   }else{
