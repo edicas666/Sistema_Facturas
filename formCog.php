eef8145a28107bbb3c9b95e709d6d0ad8cbda1b2
harriroot <ing.huzzidiel@gmail.com>
2016-03-03 10:13:48 -0600

    registrarCog
    Falta pulir detalles

--- formCog.php
+++ formCog.php
@@ -10,31 +10,31 @@
   <title>Nuevo Registro COG</title>
 </head>
 <body>
-    <form class="form-horizontal" action="crear_usuarios.php" method="post" id="form">
+    <form class="form-horizontal" action="registros/registrarCog.php" method="post" id="form">
       <h1>Registrar nuevo COG</h1>
       <br>
       <div class="form-group">
         <label for="nombre" class="col-sm-2">* Cuenta:</label>
         <div class="col-sm-10">
-          <input id="idCuenta" type="text" name="nombre" value="" placeholder="ejemplo: 10000-11301"
+          <input id="idCuenta" type="text" name="cuenta" value="" placeholder="ejemplo: 10000-11301"
           class="form-control" onKeyUp="javascript:validarCuenta('idCuenta')" maxlength="50">
         </div>
       </div>
       <div class="form-group">
         <label for="apaterno" class="col-sm-2">* Descripción de cuenta:</label>
         <div class="col-sm-10">
-          <input type="text" name="apaterno" value="" placeholder="ejemplo: Sueldos base" class="form-control" onkeypress="LetrasEspacios()" maxlength="40">
+          <input type="text" name="descripción" value="" placeholder="ejemplo: Sueldos base" class="form-control" onkeypress="LetrasEspacios()" maxlength="40">
         </div>
       </div>
       <div class="form-group">
         <label for="amaterno" class="col-sm-2">* Capitulo:</label>
         <div class="col-sm-10">
-          <input type="text" name="amaterno" value="" placeholder="ejemplo: 1000" class="form-control" onkeypress="validarNumeros()" maxlength="6">
+          <input type="text" name="capitulo" value="" placeholder="ejemplo: 1000" class="form-control" onkeypress="validarNumeros()" maxlength="6">
         </div>
       </div>
       <div class="form-group">
         <div class="col-sm-4 col-sm-offset-2">
-          <button id="registrarbtn" type="button" class="btn btn-success" onclick="registrar()">Registrar</button>
+          <button id="registrarbtn" type="submit" class="btn btn-success" >Registrar</button>
           <button type="button" onclick="window.location='index.php'" class="btn btn-default">Regresar</button>
         </div>
       </div>
@@ -46,8 +46,6 @@
 </html>
 <script type="text/javascript">
   function registrar(){
-    if(confirm("¿Esta seguro?")){
-      document.getElementById('form').submit();
-    }
+   
   }
 </script>
---
+++ registros/registrarCog.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/inicio_sesion.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <script src="../js/funciones.js"></script>
+    <title>Acceso</title>
+  </head> 
+  <body>
+   <?php
+   include_once "funciones.php";
+   $link = Conectarse();
+   if(isset($_POST['cuenta']) && !empty($_POST['cuenta']) && isset($_POST['descripción']) && !empty($_POST['descripción']) && isset($_POST['capitulo']) && !empty($_POST['capitulo']) ){
+   	  $cuenta = $_POST['cuenta'];
+      $nombre =  $_POST['descripción']; 
+      $capitulo = $_POST['capitulo'];
+      $sql = "INSERT INTO cog(id,nombre,capitulo) values('$cuenta','$nombre','$capitulo')";
+      mysql_query($sql,$link);
+      header('Location: ../formCog.php');
+   }else{
+      echo "debe de llenar todos los campos";
+   }
+    ?>
+  </body>
+</html>
